f7e4021859377406c4d9e24dcf0bed2930141b08
Mike Anderson <mike@echosec.net>
2017-06-01 22:19:14 -0700

    modified service to return just code json and accept timestamp for DST adjustment

--- routes/timezone.js
+++ routes/timezone.js
@@ -5,14 +5,25 @@ var router = express.Router();
 const latValidate = require('../util/coord-validation').validateLatitude;
 const lngValidate = require('../util/coord-validation').validateLongitude;
 const featuresMapper = require('../util/mappers').featuresMapper;
+const propertiesMapper = require('../util/mappers').propertiesMapper;
 
-/**
- * Returns GeoJSON if the requested coordinates are within a timezone, else a 400 error.
- *
- * Url parameters are lng and lat, representing longitude and latitude.
- * @returns {object} A response object
- */
-router.get('/', function(req, res, next) {
+function queryPoint (req, lng, lat, cb) {
+
+  const point = {
+    "type": "Point",
+    "coordinates": [lng, lat]
+  };
+
+  req.timezoneStore.contains(point, cb);
+}
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+router.use(function (req, res, next) {
   const lng = req.query.lng;
   const lat = req.query.lat;
 
@@ -26,25 +37,57 @@ router.get('/', function(req, res, next) {
     return res.status(400).send('Malformed longitude or latitude values');
   }
 
-  const point = {
-    "type": "Point",
-    "coordinates": [lng, lat]
-  };
+  next();
+});
 
-  req.timezoneStore.contains(point, function(error, features) {
-
+/**
+ * Returns GeoJSON if the requested coordinates are within a timezone, else a 400 error.
+ *
+ * Url parameters are lng and lat, representing longitude and latitude.
+ * @returns {object} A response object
+ */
+router.get('/geoJSON', function(req, res, next) {
+  const lng = req.query.lng;
+  const lat = req.query.lat;
+  const timestamp = req.query.timestamp;
+  queryPoint(req, lng, lat, function(error, features) {
+    
     if (error) {
       return res.status(500);
     }
 
     if (features) {
       // Adds the timezone code and formats the response we are expecting.
-      features = featuresMapper(features, lng, lat);
+      features = featuresMapper(features, lng, lat, timestamp);
     }
 
     res.json(features);
   });
+});
 
+/**
+ * Returns JSON if the requested coordinates are within a timezone, else a 400 error.
+ *
+ * Url parameters are lng and lat, representing longitude and latitude.
+ * @returns {object} A response object
+ */
+router.get(['/json', '/'], function(req, res, next) {
+  const lng = req.query.lng;
+  const lat = req.query.lat;
+  const timestamp = req.query.timestamp;
+  queryPoint(req, lng, lat, function(error, features) {
+    
+    if (error) {
+      return res.status(500);
+    }
+
+    if (features) {
+      // Adds the timezone code and formats the response we are expecting.
+      features = propertiesMapper(features, lng, lat, timestamp);
+    }
+
+    res.json(features);
+  });
 });
 
 module.exports = router;
--- util/mappers.js
+++ util/mappers.js
@@ -13,11 +13,21 @@ module.exports = (function() {
    * @param {number} lat The latitude to append to the response.
    * @returns {object} A GeoJSON feature with timezone support and renamed properties.
    */
-  const featuresMapper = function(features, lng, lat) {
+  const featuresMapper = function(features, lng, lat, timestamp) {
     features.map(feature => {
-
+    
+      const tz = timestamp 
+                  ? moment.tz(`${timestamp}`, 'X', feature.properties.TZID) 
+                  : moment().tz(feature.properties.TZID);
+      
       // Get the timezone abbreviation
-      feature.properties.code = moment().tz(feature.properties.TZID).zoneAbbr();
+      feature.properties.code = tz.zoneAbbr();
+
+      // Get the timezone offset
+      feature.properties.offset = tz.format('Z');
+
+      // Get the timezone offset in minutes
+      feature.properties.offset_seconds = tz.utcOffset() * 60;
 
       // Return the original requested coordinates
       feature.properties.coords = [lng, lat];
@@ -33,7 +43,33 @@ module.exports = (function() {
     return features;
   };
 
+  /**
+   * Adds timezone codes to a all properties for each feature in the feature collection.
+   *
+   * @param {object[]} features An array of GeoJson features.
+   * @param {number} lng The longtitude to append to the response.
+   * @param {number} lat The latitude to append to the response.
+   * @param {string} timestamp The time for determining daylight savings time
+   * @returns {object} A collection of object literals with renamed properties.
+   */
+  const propertiesMapper = function(features, lng, lat, timestamp) {
+    return features.map(feature => {
+      const tz = timestamp 
+                  ? moment.tz(`${timestamp}`, 'X', feature.properties.TZID) 
+                  : moment().tz(feature.properties.TZID);
+ 
+      return {
+        code: tz.zoneAbbr(),
+        offset: tz.format('Z'),
+        offset_seconds: tz.utcOffset() * 60,
+        coords: [lng, lat],
+        timezone: feature.properties.TZID
+      };
+    });
+  };
+
   return {
-    featuresMapper: featuresMapper
+    featuresMapper: featuresMapper,
+    propertiesMapper: propertiesMapper 
   };
 }());
